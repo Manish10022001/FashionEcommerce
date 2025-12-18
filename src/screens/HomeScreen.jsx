3b764cc8afb174c12d8cae27f3440f5f76564fc6
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-18 17:39:33 +0530

    search input field

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, TextInput, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function HomeScreen() {
   return (
@@ -9,7 +10,13 @@ export default function HomeScreen() {
       <SafeAreaView style={{ flex: 1 }}>
         <LinearGradient colors={["#f0fdf6", "#e6f7ef"]} style={styles.container}>
             <Header />
-          <Text>Home Screen</Text>
+          <Text style={styles.matchText}>Match Your Style</Text>
+          <View style={styles.inputContainer}>
+            <View style={styles.iconContainer}>
+                <Ionicons name="search" size={26} color={"#C0C0C0"}/>
+            </View>
+            <TextInput style={styles.TextInput} placeholder="search"/>
+          </View>
         </LinearGradient>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -21,4 +28,23 @@ const styles = StyleSheet.create({
     flex: 1,
     padding:20,
   },
+  matchText:{
+    fontSize:28,
+    color:"#000000",
+    marginTop:25,
+  },
+  inputContainer:{
+    backgroundColor:"#FFFFFF",
+    height:48,
+    borderRadius:12,
+    flexDirection:"row",
+    alignItems:"center",
+    marginVertical:20,
+  },
+  iconContainer:{
+    marginHorizontal:20,
+  },
+  TextInput:{
+    flex:1
+  }
 });
