8a77362d6577e1776659f94eccbc996c5d598ca1
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-18 14:01:23 +0530

    Project, Navigation and Vector icons setup

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import { Ionicons } from '@expo/vector-icons';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon:({color})=><Ionicons name='home' size={25} color={color} />
+        }}/>
+        <Tab.Screen name="Reorder" component={HomeScreen} options={{
+        tabBarIcon:({color})=><Ionicons name='reorder-four' size={25} color={color} />
+        }}/>
+        <Tab.Screen name="Cart" component={HomeScreen} options={{
+        tabBarIcon:({color})=><Ionicons name='cart' size={25} color={color} />
+        }}/>
+        <Tab.Screen name="Account" component={HomeScreen} options={{
+        tabBarIcon:({color})=><Ionicons name='person' size={25} color={color} />
+        }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
